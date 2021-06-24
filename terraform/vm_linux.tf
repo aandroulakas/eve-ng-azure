92f25c519e01dbb3b4f01f2408c8b56e83d829ad
Antonis Androulakakis <antonis.androulakakis@antoniss-mbp.home>
2021-06-24 22:33:52 +0100

    change VM size

--- terraform/vm_linux.tf
+++ terraform/vm_linux.tf
@@ -53,7 +53,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 }
