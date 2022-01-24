ba03698c9f1d72b1a9055f17d9830f19abbaf5f2
Antonis Androulakakis <aandroulakakis@aamac.home>
2022-01-24 14:09:14 +0000

    Admin SSH key to RSA

--- terraform/admin_key.tf
+++ terraform/admin_key.tf
@@ -2,6 +2,6 @@ resource "azurerm_ssh_public_key" "main" {
   name                = "admin_ssh_key"
   resource_group_name = azurerm_resource_group.main.name
   location            = var.location
-  public_key          = file("files/id_ed25519.pub")
+  public_key          = file("files/id_rsa.pub")
   tags                = var.tags
 }
